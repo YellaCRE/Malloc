1f075989ec34a9c5d15563e9e9e655e33818aa8c
Changjo Hwang <hchj2005@gmail.com>
2023-11-16 11:15:48 +0900

    refactor: 주석 추가

--- mmBinary.c
+++ mmBinary.c
@@ -389,7 +389,7 @@ void *place(void *bp, size_t asize, bool trivial_split) {
       PUT(HDRP(next), PACK(next_size, 0));
       PUT(FTRP(next), PACK(next_size, 0));
     } 
-    // next size가 6워드 이상이면 coalesce에서 뒷 블록과 합쳐지지 않게 하기 위해서(합쳐지면 너무 커진다) free블록을 앞에 위치하게 한다
+    // next size가 6워드 이상이면 앞 쪽 free block과 합쳐서 트리에 넣음으로써 단편화를 해소할 수 있는 것 같다
     else {
       next = bp;
       SET_SIZE(HDRP(next), next_size);
